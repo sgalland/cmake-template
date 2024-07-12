419fe96ba7f3bd533e5c694c7bb984351a3766dd
Sean Galland <s_galland@msn.com>
2024-07-12 11:04:39 -0600

    Updated the ability to add the library to the exe project

--- exe/src/main.c
+++ exe/src/main.c
@@ -1,12 +1,24 @@
+#include "bool.h"
 #include "file.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
 
-  /*bool p = false;
+  bool p = false;
 
   printf("p = %s\n", p ? "true" : "false");
-*/
+
+  FILE *fp;
+
+  if (!FILE_Open("main.c", "rb", fp)) {
+    ferror(fp);
+    exit(0);
+  }
+
+  if (fp != NULL)
+    fclose(fp);
+
   printf("boo\n");
   return 0;
 }
